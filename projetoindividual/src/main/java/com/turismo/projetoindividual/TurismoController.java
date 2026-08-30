89809e17035ce5752bb4d63ada7e9ee76e21ea61
Lucasmaximo0 <lucas.maximo@sptech.school>
2026-08-29 23:17:19 -0300

    feat: implementando GET e POST

--- projetoindividual/src/main/java/com/turismo/projetoindividual/TurismoController.java
+++ projetoindividual/src/main/java/com/turismo/projetoindividual/TurismoController.java
@@ -1,11 +1,31 @@
 package com.turismo.projetoindividual;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/Destino")
+
 public class TurismoController {
+
+    private List<Destino> destinos = new ArrayList<>();
+    private  Integer contador = 0;
+
+    @PostMapping
+    public  Destino cadastrar(@RequestBody Destino destino){
+        destino.setId(++contador);
+        destinos.add(destino);
+
+        return destino;
+
+    }
+
+    @GetMapping
+    public List<Destino> listar(){
+        return  destinos;
+    }
 
 
 
