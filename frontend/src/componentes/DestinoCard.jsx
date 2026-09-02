b981f0f8216ba83594d596fbff8e5de19c56cfa0
Lucasmaximo0 <lucas.maximo@sptech.school>
2026-09-02 00:49:23 -0300

    feat: Adiciona exclusão de destinos

--- frontend/src/componentes/DestinoCard.jsx
+++ frontend/src/componentes/DestinoCard.jsx
@@ -1,4 +1,17 @@
-function DestinoCard({ destino }) {
+    import axios from "axios"
+
+    function DestinoCard({ destino }) {
+
+    function excluirDestino() {
+
+        axios.delete(`http://localhost:8080/Destino/${destino.id}`)
+            .then(() => {
+                alert("Destino excluído com sucesso!")
+            })
+            .catch(erro => {
+                console.log("Não foi possível excluir o destino:", erro)
+            })
+    }    
 
     return (
         <div>
@@ -7,6 +20,12 @@ function DestinoCard({ destino }) {
             <p>Estado: {destino.estado}</p>
             <p>Descrição: {destino.descricao}</p>
             <p>Preço médio: R$ {destino.precoMedio}</p>
+
+            <button onClick={excluirDestino}>
+                🗑️ Excluir
+            </button>
+
+
         </div>
     )
 }
